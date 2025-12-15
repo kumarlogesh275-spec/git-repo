f0540df63ef598e3d151f87d570401bfa5a78d4d
kumarlogesh275 <kumarlogesh275@gmail.com>
2025-12-14 22:48:33 -0800

    third

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "loki2001" # Replace with your actual S3 bucket name
+    bucket = "ttf10" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-west-2"
   }
